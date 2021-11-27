cea5699ff884a8cf2a9a4bb97be6cbb630617cfd
Felipe Peres <gpsouza.felipe@gmail.com>
2021-11-27 07:54:10 -0300

    Success!

--- src/App.js
+++ src/App.js
@@ -1,26 +1,53 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from './services/api.js';
+
 function App() {
+
+  const [movies, setMovies] = useState([]);
+
   async function handleAddRepository() {
-    // TODO
+
+    const response = await api.post('movies', {
+
+      title: "Como montar um painel",
+      description: "Neste vídeo você irá aprender como montar um painel.",
+      tags : ["automacao", "painel", "montar"],
+      time : 1800,
+      default_image : "amazon.com.br/tananan",
+      file : "amazon.com.br/tananan"
+    });
+
+    const movie = response.data;
+    setMovies([...movies, movie]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`movies/${id}`);
+
+    setMovies(movies.filter(movie => movie.id !== id));      
+    
   }
+
+  useEffect(() => {
+    api.get('movies').then(response => {
+    setMovies(response.data);
+    });
+  }, []);
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
+        
+        {movies.map(movie => 
+        <li key={movie.id}>{movie.title}
+          <button onClick={() => handleRemoveRepository(movie.id)}>
             Remover
           </button>
-        </li>
+        </li>)}
+
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
